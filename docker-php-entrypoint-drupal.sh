12da1c4a6ee1507e11de8b3ca43db2a4c0c65ffa
Michael Büchner <m.buechner@dnb.de>
2020-06-22 13:40:46 +0200

    Updated docker-php-entrypoint-drupal.sh

--- docker-php-entrypoint-drupal.sh
+++ docker-php-entrypoint-drupal.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set +e
 echo "Start Drupal Update DB..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web updb
+/var/www/html/vendor/bin/drush --root /var/www/html/web updatedb
 echo "Start Drupal Clear Cache..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web cc all
+/var/www/html/vendor/bin/drush --root /var/www/html/web cache:clear all
 echo "Run Drupal's Cron..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web cron
+/var/www/html/vendor/bin/drush --root /var/www/html/web core:cron
 set -e
 
 docker-php-entrypoint
