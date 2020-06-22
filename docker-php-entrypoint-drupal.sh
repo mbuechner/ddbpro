8c2d38cc1fca488a07903b338e75fff2abac88fa
Michael Büchner <m.buechner@dnb.de>
2020-06-22 14:12:58 +0200

    Added always-yes-option to Drush commands

--- docker-php-entrypoint-drupal.sh
+++ docker-php-entrypoint-drupal.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set +e
 echo "Start Drupal Update DB..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web updatedb
+/var/www/html/vendor/bin/drush --root /var/www/html/web -y updatedb
 echo "Start Drupal Clear Cache..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web cache:clear all
+/var/www/html/vendor/bin/drush --root /var/www/html/web -y cache:clear all
 echo "Run Drupal's Cron..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web core:cron
+/var/www/html/vendor/bin/drush --root /var/www/html/web -y core:cron
 set -e
 
 docker-php-entrypoint
